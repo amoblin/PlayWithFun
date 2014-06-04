2567f8e661017695a37a3d9b9ec76d82f7b95609
amoblin@mpb <amoblin@gmail.com>
2014-06-04 16:10:24 +0800

    Initial Commit

---
+++ PlayWithFun/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PlayWithFun
+//
+//  Created by amoblin on 14/6/4.
+//  Copyright (c) 2014年 amoblin. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PlayWithFun/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PlayWithFun
+//
+//  Created by amoblin on 14/6/4.
+//  Copyright (c) 2014年 amoblin. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
